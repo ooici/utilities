7bea5b0ae927b45c18f11666b59fc72349db7b01
Jonathan Newbrough <jonathan.newbrough@gyregroup.com>
2012-09-06 10:33:34 -0700

    make it an egg

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(name='utilities',
+    version='2012.9.6',
+    author='Jonathan Newbrough',
+    author_email='jonathan.newbrough@gyregroup.com',
+    url='https://github.com/newbrough/utilities',
+    package_dir = {'': 'src'},
+    packages=['ooi', 'ooi.logging'],
+    install_requires = [ 'pyyaml==3.10', 'graypy' ]
+)
+
