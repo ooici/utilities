b859334c55f0ed41b11ef7399bc0b21bc4c82f10
Jonathan Newbrough <jonathan.newbrough@gyregroup.com>
2013-06-11 09:40:24 -0700

    create test to uncover import issues, see OOIION-992

--- src/ooi/testing.py
+++ src/ooi/testing.py
@@ -1,10 +1,23 @@
 """
-testing utilities
+testing utilities to check that modules can be imported
+
+these are not tests directly -- see implementations in coi-services/core/test/test_project_imports.py
+and pyon/core/test/test_project_imports.py
+
+imports cannot be safely un-imported, so if you run test_can_import below, you cannot run test_can_import_from_any_dir in the same go.
+so to use, run one method per command.
+
+examples from pyon:
+    bin/nosetests pyon/core/test/test_project_imports.py:TestProjectImports.test_can_import
+    bin/nosetests pyon/core/test/test_project_imports.py:TestProjectImports.test_can_import_from_any_dir
+
+examples from coi-services:
+    bin/nosetests ion/core/test/test_project_imports.py:TestProjectImports.test_can_import
+    bin/nosetests ion/core/test/test_project_imports.py:TestProjectImports.test_can_import_from_any_dir
 """
 
 import unittest
 import os
-import os.path
 
 class ImportTest(unittest.TestCase):
     """
@@ -20,7 +33,7 @@ class ImportTest(unittest.TestCase):
         super(ImportTest,self).__init__(*a,**b)
         self.source_directory = source_directory
         self.base_package = base_package
-
+        print 'source dir: %s\npkgs: %s' % (source_directory, base_package)
     def test_can_import(self):
         failures = []
 
@@ -31,23 +44,29 @@ class ImportTest(unittest.TestCase):
         if failures:
             self.fail(msg='failed to import these modules:\n' + '\n'.join(failures))
 
+    def test_can_import_from_any_dir(self):
+        original_dir = os.getcwd()
+        try:
+            os.chdir('/tmp')
+            self.test_can_import()
+        finally:
+            os.chdir(original_dir)
+
     def _import_below(self, dir, mod, failures):
         subdir = os.path.join(self.source_directory, dir)
         for entry in os.listdir(subdir):
             path = os.path.join(subdir, entry)
-            # each python script (except init), try to import
-            if os.path.isfile(path) and entry.endswith('.py') and entry!='__init__.py':
-                try:
+            try:
+                # each python script (except init), try to import
+                if os.path.isfile(path) and entry.endswith('.py') and entry!='__init__.py':
                     submod = mod + '.' + entry[:-3]
                     __import__(submod)
-                except Exception,e:
-                    failures.append(submod + '\t' + str(e))
-            # each new subpackage import and recurse
-            elif os.path.isdir(path) and os.path.isfile(os.path.join(path,'__init__.py')) and entry!='test':
-                submod = mod + '.' + entry
-                try:
+                    del submod
+                # each new subpackage import and recurse
+                elif os.path.isdir(path) and os.path.isfile(os.path.join(path,'__init__.py')) and entry!='test':
+                    submod = mod + '.' + entry
                     __import__(submod)
-                except Exception,e:
-                    failures.append(submod + '\t' + str(e))
-                    continue
-                self._import_below(os.path.join(dir,entry), submod, failures)
+                    self._import_below(os.path.join(dir,entry), submod, failures)
+            except Exception,e:
+                failures.append(submod)
+
